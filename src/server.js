0d24e40f208f7955747dd3ff93097d9854b0c5b6
Priyansusahoo <priyansusahoo1@gmail.com>
2025-03-22 14:17:17 +0530

    vercel deployment v3

--- src/server.js
+++ src/server.js
@@ -2,38 +2,12 @@ const app = require("./app");
 const connectDB = require("./config/db");
 
 require("dotenv").config();
-// const PORT = process.env.PORT;
 
-// process.on("uncaughtException", (err) => {
-//     console.error(`Uncaught Exception: ${err.message}`);
-//     process.exit(1);
-// });
+connectDB()
+  .then(() => console.log("✅ Database connected successfully!"))
+  .catch((error) => {
+    console.error("❌ Database connection failed:", error);
+    process.exit(1);
+  });
 
-// connectDB()
-//     .then(() => {
-//         app.listen(PORT, () => {
-//             console.log(`✅ Server running on port ${PORT}`);
-//         });
-//     })
-//     .catch((error) => {
-//         console.error("❌ Database connection failed:", error);
-//         process.exit(1);
-//     });
-
-// process.on("unhandledRejection", (err) => {
-//     console.error(`Unhandled Rejection: ${err.message}`);
-//     process.exit(1);
-// });
-const startServer = async () => {
-    try {
-        await connectDB();
-        console.log("✅ Database connected successfully");
-
-        module.exports = app; // Export app for Vercel (No app.listen())
-    } catch (error) {
-        console.error("❌ Database connection failed:", error);
-        process.exit(1);
-    }
-};
-
-startServer();
+module.exports = app;
